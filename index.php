605c874995b09fb83307fa2eb24ef0a2cd0f03f0
shoaib0300 <shoaibsarwar187@gmail.com>
2024-06-08 19:35:43 +0200

    creating a pop-up

--- index.php
+++ index.php
@@ -44,7 +44,8 @@
                 <h4 class="mt-2 text-primary">All user in database</h4>
             </div>
             <div class="col-lg-6">
-                <button type="button" class="btn btn-primary m-1 float-right"><i class="fa-sharp fa-solid fa-user-plus"></i> &nbsp;&nbsp;Add a new User</button>
+                <button type="button" class="btn btn-primary m-1 float-right" data-toggle="modal" data-target="#addUserModal">
+                    <i class="fa-sharp fa-solid fa-user-plus"></i> &nbsp;&nbsp;Add a new User</button>
                 <a href="#" class="btn btn-success m-1 float-right">Export to Excel</a>   
             </div>
         </div>
@@ -90,6 +91,41 @@
             </div>
         </div>
     </div>
+    <!-- Add new User model -->
+    <div class="modal fade" id="addUserModal">
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+      
+        <!-- Modal Header -->
+        <div class="modal-header">
+          <h4 class="modal-title">Add new User</h4>
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+        </div>
+        
+        <!-- Modal body -->
+        <div class="modal-body px-4">
+            <form action="" method="post" id="form-data">
+                <div class="form-group">
+                    <input type="text" name="fname" class="form-control" placeholder="First Name" required>
+                </div>
+                <div class="form-group">
+                    <input type="text" name="lname" class="form-control" placeholder="Last Name" required>
+                </div>
+                <div class="form-group">
+                    <input type="email" name="email" class="form-control" placeholder="Email" required>
+                </div>
+                <div class="form-group">
+                    <input type="tel" name="phone" class="form-control" placeholder="Phone NUmber" required>
+                </div>
+                <div class="form-group">
+                    <input type="submit" name="insert" id="insert" value="Add User" class="btn btn-danger btn-block" placeholder="First Name" required>
+                </div>
+            </form>
+        </div>
+      </div>
+    </div>
+  </div>
+  
 
 
 
@@ -104,5 +140,10 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.datatables.net/v/bs4/dt-2.0.8/datatables.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script type="text/javascript">
+        $(document).ready(function(){
+            $("table").DataTable();
+        });
+    </script>
 </body>
 </html>
