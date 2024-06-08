960c57e1edd025cdc25eb1ca7cf7487bd7526f4e
shoaib0300 <shoaibsarwar187@gmail.com>
2024-06-08 21:59:59 +0200

    Modals Created

--- index.php
+++ index.php
@@ -4,15 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP OOP</title>
-
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
     <link href="https://cdn.datatables.net/v/bs4/dt-2.0.8/datatables.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-
 </head>
 <body>
-
-
+        <!-- Navbar -->
     <nav class="navbar navbar-expand-md bg-dark navbar-dark">
     <a class="navbar-brand" href="#"><i class="fab fa-wolf-pack-battalion">Crud</i></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
@@ -32,7 +29,7 @@
         </ul>
     </div>
     </nav>
-
+        <!-- table -->
     <div class="container">
         <div class="row">
             <div class="col-lg-12">
@@ -94,47 +91,35 @@
     <!-- Add new User model -->
     <div class="modal fade" id="addUserModal">
     <div class="modal-dialog modal-dialog-centered">
-      <div class="modal-content">
-      
-        <!-- Modal Header -->
-        <div class="modal-header">
-          <h4 class="modal-title">Add new User</h4>
-          <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <div class="modal-content">
+            <!-- Modal Header -->
+            <div class="modal-header">
+                <h4 class="modal-title">Add new User</h4>
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+            </div>
+            <!-- Modal body -->
+            <div class="modal-body px-4">
+                <form action="" method="post" id="form-data">
+                    <div class="form-group">
+                        <input type="text" name="fname" class="form-control" placeholder="First Name" required>
+                    </div>
+                    <div class="form-group">
+                        <input type="text" name="lname" class="form-control" placeholder="Last Name" required>
+                    </div>
+                    <div class="form-group">
+                        <input type="email" name="email" class="form-control" placeholder="Email" required>
+                    </div>
+                    <div class="form-group">
+                        <input type="tel" name="phone" class="form-control" placeholder="Phone NUmber" required>
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" name="insert" id="insert" value="Add User" class="btn btn-danger btn-block" placeholder="First Name" required>
+                    </div>
+                </form>
+            </div>
         </div>
-        
-        <!-- Modal body -->
-        <div class="modal-body px-4">
-            <form action="" method="post" id="form-data">
-                <div class="form-group">
-                    <input type="text" name="fname" class="form-control" placeholder="First Name" required>
-                </div>
-                <div class="form-group">
-                    <input type="text" name="lname" class="form-control" placeholder="Last Name" required>
-                </div>
-                <div class="form-group">
-                    <input type="email" name="email" class="form-control" placeholder="Email" required>
-                </div>
-                <div class="form-group">
-                    <input type="tel" name="phone" class="form-control" placeholder="Phone NUmber" required>
-                </div>
-                <div class="form-group">
-                    <input type="submit" name="insert" id="insert" value="Add User" class="btn btn-danger btn-block" placeholder="First Name" required>
-                </div>
-            </form>
-        </div>
-      </div>
     </div>
-  </div>
-  
-
-
-
-
-
-
-
-
-
+    <!-- Scripts -->
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
